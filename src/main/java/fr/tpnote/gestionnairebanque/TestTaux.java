f7fc834d9921ea86bbe45428150ed585b9b99c8f
Julien ADAMI <adamij@3il.fr>
2023-03-31 14:04:59 +0200

    fin de la classe, les getter et setters

--- src/main/java/fr/tpnote/gestionnairebanque/TestTaux.java
+++ src/main/java/fr/tpnote/gestionnairebanque/TestTaux.java
@@ -2,11 +2,68 @@ package fr.tpnote.gestionnairebanque;
 
 import org.junit.Test;
 
-public class TestTaux {
+import static org.junit.Assert.assertEquals;
 
+public class TestTaux {
+    Taux taux;
     @Test
     public void testInitialisationTaux(){
-        Taux taux = new Taux(23d,20d,25d);
+         taux = new Taux("nom",0,10,0.1);
     }
+
+    @Test
+    public void testGetTauxInferieur(){
+         taux = new Taux("nom",0,10,0.1);
+        assertEquals(taux.getTauxInferieur(),0,0);
+    }
+
+    @Test
+    public void testGetTauxSuperieur(){
+         taux = new Taux("nom",0,10,0.1);
+        assertEquals(taux.getTauxSuperieur(),10,0);
+    }
+
+    @Test
+    public void testGetTauxAApliquer(){
+         taux = new Taux("nom",0,10,0.1);
+        assertEquals(taux.getTauxAApliquer(),0.1,0);
+    }
+
+    @Test
+    public void testGetNomTaux(){
+         taux = new Taux("nom",0,10,0.1);
+        assertEquals(taux.getNomTaux(),"nom");
+    }
+
+    @Test
+    public void testSetTauxInferieur(){
+         taux = new Taux("nom",0,10,0.1);
+        taux.setTauxInferieur(1);
+        assertEquals(taux.getTauxInferieur(),1,0);
+    }
+
+    @Test
+    public void testSetTauxSuperieur(){
+         taux = new Taux("nom",0,10,0.1);
+        taux.setTauxSuperieur(1);
+        assertEquals(taux.getTauxSuperieur(),1,0);
+    }
+
+    @Test
+    public void testSetTauxAApliquer(){
+         taux = new Taux("nom",0,10,0.1);
+        taux.setTauxAApliquer(1);
+        assertEquals(taux.getTauxAApliquer(),1,0);
+    }
+
+    @Test
+    public void testSetNomTaux(){
+         taux = new Taux("nom",0,10,0.1);
+        taux.setNomTaux("nom2");
+        assertEquals(taux.getNomTaux(),"nom2");
+    }
+
+
+
 
 }
