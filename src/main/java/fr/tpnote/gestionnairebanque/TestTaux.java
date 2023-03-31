52e58237fe93b2a16884dd0a8793a1ea55c96769
Julien ADAMI <adamij@3il.fr>
2023-03-31 13:59:04 +0200

    Ajout du testtestInitialisationTaux qui ne passe pas car pas de constructeur

--- src/main/java/fr/tpnote/gestionnairebanque/TestTaux.java
+++ src/main/java/fr/tpnote/gestionnairebanque/TestTaux.java
@@ -1,4 +1,12 @@
 package fr.tpnote.gestionnairebanque;
 
+import org.junit.Test;
+
 public class TestTaux {
+
+    @Test
+    public void testInitialisationTaux(){
+        Taux taux = new Taux(23d,20d,25d);
+    }
+
 }
