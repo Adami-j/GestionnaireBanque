8ada1423a88e9bddca5b18142de4a77319ec0ee8
Julien ADAMI <adamij@3il.fr>
2023-03-31 13:44:20 +0200

    Modification de l'application : nom de la fenêtre, redimentionnement false, fond lightgrey, taille

--- src/main/java/fr/tpnote/gestionnairebanque/GestionnaireBanqueApplication.java
+++ src/main/java/fr/tpnote/gestionnairebanque/GestionnaireBanqueApplication.java
@@ -7,12 +7,14 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class GestionnaireBanqueApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(GestionnaireBanqueApplication.class.getResource("IHM.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 200);
+        stage.setTitle("Gestion bancaire");
+        stage.setResizable(false);
+        scene.getRoot().setStyle("-fx-background-color: lightgrey;");
         stage.setScene(scene);
         stage.show();
     }
