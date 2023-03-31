08a6e8a070dc9656e50ef09c2b4d774b8c9144a0
Julien ADAMI <adamij@3il.fr>
2023-03-31 14:31:43 +0200

    initialisation de la liste transaction dans le constructeur

--- src/main/java/fr/tpnote/gestionnairebanque/GestionnaireBancaire.java
+++ src/main/java/fr/tpnote/gestionnairebanque/GestionnaireBancaire.java
@@ -1,10 +1,15 @@
 package fr.tpnote.gestionnairebanque;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class GestionnaireBancaire {
 
-    private List<Transaction> listeTransaction;
+    private List<Transaction> listeTransaction = null;
+
+    public GestionnaireBancaire() {
+        this.listeTransaction = new ArrayList<>();
+    }
 
     public List<Transaction> getListeTransaction() {
         return listeTransaction;
