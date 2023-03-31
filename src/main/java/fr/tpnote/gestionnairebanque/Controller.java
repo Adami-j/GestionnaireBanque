07277b3959b86a62aecd23437084805ada51ef1b
Julien ADAMI <adamij@3il.fr>
2023-03-31 15:50:57 +0200

    lien entre les éléments graphiques et le code ajout de la méthode initilize pour l'observable list

--- src/main/java/fr/tpnote/gestionnairebanque/Controller.java
+++ src/main/java/fr/tpnote/gestionnairebanque/Controller.java
@@ -1,5 +1,37 @@
 package fr.tpnote.gestionnairebanque;
 
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
 public class Controller {
+    GestionnaireBancaire banque = new GestionnaireBancaire();
+    @FXML
+    private TextField nom;
+    @FXML
+    private TextField montant;
+    @FXML
+    private Label solde;
+
+    @FXML
+    ListView<Transaction> listeTransaction= new ListView<>();
+
+    @FXML
+    ComboBox comboboxid;
+
+    @FXML
+    Button enregistrer;
+
+    @FXML
+    public void initialize(){
+        ObservableList<Transaction> items = listeTransaction.getItems();
+
+    }
+
+    @FXML
+    public void actionEnregistrer(){
+
+    }
+
 
 }
