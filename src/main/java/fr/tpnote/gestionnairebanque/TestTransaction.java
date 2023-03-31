6b66984e16aea41627ad11086441a15cb102ce00
Julien ADAMI <adamij@3il.fr>
2023-03-31 13:55:49 +0200

    Ajout de tous les tests naifs

--- src/main/java/fr/tpnote/gestionnairebanque/TestTransaction.java
+++ src/main/java/fr/tpnote/gestionnairebanque/TestTransaction.java
@@ -2,12 +2,53 @@ package fr.tpnote.gestionnairebanque;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class TestTransaction {
 
+    Transaction transaction;
     @Test
     public void testInitialisationBanque(){
-        Transaction transaction = new Transaction("nom",'c',15d);
-
+       transaction = new Transaction("nom",'c',15d);
+        assertEquals(transaction.getNom(),"nom");
     }
+
+    @Test
+    public void testGetType(){
+       transaction = new Transaction("nom",'c',15d);
+        assertEquals(transaction.getType(),'c');
+    }
+
+    @Test
+    public void testGetValeur(){
+       transaction = new Transaction("nom",'c',15d);
+        assertEquals(transaction.getValeur(),15d,0);
+    }
+
+    @Test
+    public void testSetNom(){
+       transaction = new Transaction("nom",'c',15d);
+       transaction.setNom("nom2");
+        assertEquals(transaction.getNom(),"nom2");
+    }
+
+    @Test
+    public void testSetType(){
+       transaction = new Transaction("nom",'c',15d);
+       transaction.setType('d');
+        assertEquals(transaction.getType(),'d');
+    }
+
+    @Test
+    public void testSetValeur(){
+       transaction = new Transaction("nom",'c',15d);
+       transaction.setValeur(20d);
+        assertEquals(transaction.getValeur(),20d,0);
+    }
+
+
+
+
+
 
 }
