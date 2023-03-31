bddc10bdb2c20ca3222566335005363f1a7e0471
Julien ADAMI <adamij@3il.fr>
2023-03-31 15:00:32 +0200

    testtransactionValeurZero bloque

--- src/main/java/fr/tpnote/gestionnairebanque/GestionnaireBancaireTest.java
+++ src/main/java/fr/tpnote/gestionnairebanque/GestionnaireBancaireTest.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
 public class GestionnaireBancaireTest {
 
@@ -37,16 +38,17 @@ public class GestionnaireBancaireTest {
     public void testtransactionNegative() {
         banque = new GestionnaireBancaire();
         banque.getListeTransaction().add(new Transaction("test", 'c', -15d));
-        banque.addTransaction(new Transaction("test", 'c', -15d));
         banque.getListeTaux().add(new Taux("test", 0, 100, 10));
-        assertEquals( banque.getListeTransaction().size(), 0);
+
+        assertThrows(IllegalArgumentException.class, () -> banque.addTransaction(new Transaction("test", 'c', -15d)));
+
     }
 
     @Test
     public void testtransactionValeurZero() {
         banque = new GestionnaireBancaire();
         banque.getListeTransaction().add(new Transaction("test", 'c', 0d));
-        assertEquals( banque.getListeTransaction().size(), 0);
+        assertEquals( banque.getListeTransaction().size(), 0d,0);
     }
 
 
