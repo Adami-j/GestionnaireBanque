926b41ab9c4d3bd444af6802ead72c033a50ea7e
Julien ADAMI <adamij@3il.fr>
2023-03-31 14:29:38 +0200

    ajout getter setter pour satisfaire le test

--- src/main/java/fr/tpnote/gestionnairebanque/GestionnaireBancaire.java
+++ src/main/java/fr/tpnote/gestionnairebanque/GestionnaireBancaire.java
@@ -1,4 +1,17 @@
 package fr.tpnote.gestionnairebanque;
 
+import java.util.List;
+
 public class GestionnaireBancaire {
+
+    private List<Transaction> listeTransaction;
+
+    public List<Transaction> getListeTransaction() {
+        return listeTransaction;
+    }
+
+    public void setListeTransaction(List<Transaction> listeTransaction) {
+        this.listeTransaction = listeTransaction;
+    }
+
 }
