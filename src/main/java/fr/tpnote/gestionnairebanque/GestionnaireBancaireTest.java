7318e2fe010cba46cd15abd75cc8d65e2920616b
Julien ADAMI <adamij@3il.fr>
2023-03-31 14:28:53 +0200

    testInitialisationGestionnaireRecupererListeTransaction ne passe pas

--- src/main/java/fr/tpnote/gestionnairebanque/GestionnaireBancaireTest.java
+++ src/main/java/fr/tpnote/gestionnairebanque/GestionnaireBancaireTest.java
@@ -1,4 +1,23 @@
 package fr.tpnote.gestionnairebanque;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
 public class GestionnaireBancaireTest {
+
+    GestionnaireBancaire banque;
+    @Test
+    public void testInitialisationGestionnaireBanque(){
+        banque = new GestionnaireBancaire();
+    }
+
+      @Test
+    public void testInitialisationGestionnaireRecupererListeTransaction() {
+          banque = new GestionnaireBancaire();
+          assertEquals( banque.getListeTransaction(), new ArrayList<Transaction>());
+      }
 }
