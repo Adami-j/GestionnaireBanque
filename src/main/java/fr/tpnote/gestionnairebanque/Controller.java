69831ec87c8c217f8427dba4d820f2a67f8c3630
Julien ADAMI <adamij@3il.fr>
2023-03-31 16:25:03 +0200

    gestion de l'ajout d'un credit ou débit

--- src/main/java/fr/tpnote/gestionnairebanque/Controller.java
+++ src/main/java/fr/tpnote/gestionnairebanque/Controller.java
@@ -1,11 +1,17 @@
 package fr.tpnote.gestionnairebanque;
 
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
+import java.io.IOException;
+
 public class Controller {
-    GestionnaireBancaire banque = new GestionnaireBancaire();
+
+    GestionnaireBancaire banque;
+
     @FXML
     private TextField nom;
     @FXML
@@ -22,16 +28,43 @@ public class Controller {
     @FXML
     Button enregistrer;
 
+    public Controller() throws IOException {
+    }
+
     @FXML
-    public void initialize(){
+    public void initialize() throws IOException {
+        banque = new GestionnaireBancaire();
         ObservableList<Transaction> items = listeTransaction.getItems();
+        ObservableList<String> itemsCombo = FXCollections.observableArrayList("Crédit", "Débit");
+        comboboxid.setItems(itemsCombo);
+        solde.setText(banque.getSolde().toString());
 
     }
 
     @FXML
-    public void actionEnregistrer(){
+    public void actionEnregistrer(ActionEvent actionEvent) {
+        char type;
+        switch (comboboxid.getValue().toString()){
+            case "Crédit":
+                type = 'c';
+                break;
+            case "Débit":
+                type = 'd';
+                break;
+            default:
+                type = 'c';
+                break;
+        }
+        Transaction t = new Transaction(nom.getText(), type, Double.parseDouble(montant.getText()));
+        banque.addTransaction(t);
+
+        listeTransaction.getItems().add(t); // Ajout de la transaction à l'observable list
+        solde.setText(banque.getSolde().toString());
+
 
     }
+
+
 
 
 }
