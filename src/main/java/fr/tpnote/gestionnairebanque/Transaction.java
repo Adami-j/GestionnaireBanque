88d4610564352bc69702206b8d8995849167ec30
Julien ADAMI <adamij@3il.fr>
2023-03-31 13:55:32 +0200

    Ajout de tous les getter setter, inutile de réaliser la classe en TDD

--- src/main/java/fr/tpnote/gestionnairebanque/Transaction.java
+++ src/main/java/fr/tpnote/gestionnairebanque/Transaction.java
@@ -1,6 +1,36 @@
 package fr.tpnote.gestionnairebanque;
 
 public class Transaction {
-    public Transaction(String nom, char c, double v) {
+    private String nom;
+    private char type;
+    private double valeur;
+    public Transaction(String nom, char type, double valeur) {
+        this.nom = nom;
+        this.type = type;
+        this.valeur = valeur;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public char getType() {
+        return type;
+    }
+
+    public void setType(char type) {
+        this.type = type;
+    }
+
+    public double getValeur() {
+        return valeur;
+    }
+
+    public void setValeur(double valeur) {
+        this.valeur = valeur;
     }
 }
